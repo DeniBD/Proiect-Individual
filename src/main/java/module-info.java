e779692897cf351b5e335618a82545f5d691bb35
Denisa <deni.buftea09@gmail.com>
2022-12-15 12:24:15 +0200

    Versiune beta

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.cinemabookingsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.sql;
 
 
     opens com.example.cinemabookingsystem to javafx.fxml;
